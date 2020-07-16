70501d5807aea15cce9f7be7a0956e8357eefda9
amirhossein asgari <ami.97.agr@gmail.com>
2020-07-16 15:46:21 +0430

    add simple boilerplate thanks to hackathon

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const logger = require('morgan')
+const path = require('path')
+const chalk = require('chalk')
+const expressStatusMonitor = require('express-status-monitor')
+const sass = require('node-sass-middleware');
 const app = express()
-const port = 3000
+
+app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.use(logger('dev'))
+app.set('view engine', 'pug')
+app.use(expressStatusMonitor())
+app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+}))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
+});
