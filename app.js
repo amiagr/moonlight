398e3c75f4936ab132ea27fe28438ff574ca3e2d
amirhossein asgari <ami.97.agr@gmail.com>
2020-07-16 16:04:59 +0430

    complete boilerplate and add home page

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const sass = require('node-sass-middleware');
 const app = express()
 
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
-app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('views', path.join(__dirname, 'views'));
-app.use(logger('dev'))
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.use(logger('dev'))
 app.use(expressStatusMonitor())
 app.use(sass({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public')
 }))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('home'))
 
 app.listen(app.get('port'), () => {
     console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
